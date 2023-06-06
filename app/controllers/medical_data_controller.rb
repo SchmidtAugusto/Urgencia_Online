48629101d84f87820d68c99b6ccc1b1544b96c5c
Augusto Schmidt <augusto_schmidt@outlook.com.br>
2023-06-06 16:03:45 -0300

    create all controllers

---
+++ app/controllers/medical_data_controller.rb
@@ -0,0 +1,2 @@
+class MedicalDataController < ApplicationController
+end
---
+++ test/controllers/coverages_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CoveragesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
