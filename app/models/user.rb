485b04feddef55aa17c50916c43b6bc8cd256f71
Augusto Schmidt <augusto_schmidt@outlook.com.br>
2023-06-06 14:05:27 -0300

    add validates from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :birthdate, :address, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :cpf, presence: true, uniqueness: true, length: { is: 11 }
 end
