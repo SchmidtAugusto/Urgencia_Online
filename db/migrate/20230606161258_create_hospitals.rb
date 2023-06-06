b2548983d922977211968f244236886d2d51359a
Augusto Schmidt <augusto_schmidt@outlook.com.br>
2023-06-06 13:13:36 -0300

    add hospital migrate

---
+++ db/migrate/20230606161258_create_hospitals.rb
@@ -0,0 +1,10 @@
+class CreateHospitals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :hospitals do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
