e27aeaecebdeb91757e64b75965bf251aa2b2e02
Augusto Schmidt <augusto_schmidt@outlook.com.br>
2023-06-07 10:27:32 -0300

    add cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,62 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_152329) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_163626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "description"
+    t.integer "color_protocol"
+    t.bigint "hospital_id", null: false
+    t.boolean "done", default: false
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hospital_id"], name: "index_appointments_on_hospital_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
+  create_table "coverages", force: :cascade do |t|
+    t.bigint "insurance_plan_id", null: false
+    t.bigint "hospital_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hospital_id"], name: "index_coverages_on_hospital_id"
+    t.index ["insurance_plan_id"], name: "index_coverages_on_insurance_plan_id"
+  end
+
+  create_table "hospitals", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "insurance_plans", force: :cascade do |t|
+    t.string "name"
+    t.integer "product"
+    t.integer "id_code"
+    t.string "plan"
+    t.integer "cns"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_insurance_plans_on_user_id"
+  end
+
+  create_table "medical_datum", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "blood_type"
+    t.string "allergies"
+    t.float "weight"
+    t.float "height"
+    t.string "health_conditions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_medical_datum_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +85,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_152329) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "appointments", "hospitals"
+  add_foreign_key "appointments", "users"
+  add_foreign_key "coverages", "hospitals"
+  add_foreign_key "coverages", "insurance_plans"
+  add_foreign_key "insurance_plans", "users"
+  add_foreign_key "medical_datum", "users"
 end
