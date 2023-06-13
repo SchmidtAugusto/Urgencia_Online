017cfb9eae9709fe2d95d4781b2d52cdc354002f
Augusto Schmidt <augusto_schmidt@outlook.com.br>
2023-06-13 15:55:10 -0300

    add js from account info form button

--- app/javascript/controllers/account_details_controller.js
+++ app/javascript/controllers/account_details_controller.js
@@ -3,9 +3,20 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="account-details"
 export default class extends Controller {
   connect() {
-    console.log("batata")
+
   }
-  disable() {
-    
+  edit(event) {
+    const inputs = document.getElementsByClassName("account-input");
+    const saveButton = document.getElementById("save-button")
+    const editButton = document.getElementById("edit-button")
+
+    event.preventDefault()
+    Array.from(inputs).forEach((input)=>{
+      input.disabled = false
+    })
+
+    saveButton.classList.remove("d-none")
+    editButton.classList.add("d-none")
+    console.log(saveButton)
   }
 }
