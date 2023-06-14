137955bcf33f9b9788fe588c7607f89afdeaefc5
Augusto Schmidt <augusto_schmidt@outlook.com.br>
2023-06-14 11:34:42 -0300

    test heroku assets

--- webpack.config.js
+++ webpack.config.js
@@ -7,21 +7,6 @@ module.exports = {
   entry: {
     application: "./app/javascript/application.js"
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
-          },
-        },
-      },
-    ],
-  },
   output: {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
@@ -33,3 +18,17 @@ module.exports = {
     })
   ]
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
