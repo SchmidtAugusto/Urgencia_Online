94c90743a13c4255153950fa4dc734634d41f7f7
Augusto Schmidt <augusto_schmidt@outlook.com.br>
2023-06-15 18:40:34 -0300

    modifier page admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -37,7 +37,7 @@ class PagesController < ApplicationController
   def admin
     if @user.admin
       authorize @user
-      @appointments = Appointment.where(hospital_id: 9, done: false)
+      @appointments = Appointment.where(hospital_id: 9, done: false).order(:id)
     else
       redirect_to root_path
     end
